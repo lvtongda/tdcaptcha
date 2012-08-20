386c91f3d8e7234c6fa1dc80e9c020973e1001e1
lvxiao <xiao.lv@upai.com>
2012-08-20 14:04:10 +0800

    Completion of the basic functions of tdCAPTCHA

---
+++ Views/captchaindex.php
@@ -0,0 +1,10 @@
+<?php
+if($pubkey = 'b7c06906733d7a4912d6d0d2faef5e50' || $pubkey == 'd800ae51a8af9790a5e9da430f@2b89'){
+        echo '<meta http-equiv="Content-Type" content="text/html;charset=utf-8" />';
+        echo '<img src="http://tdcaptcha.com/Models/ValidatorCode.php" id="code" /><br />';
+        echo '<input type="text" value="" name="scode"><br />';
+        echo '<span>看不清，<a href=javascript:document.getElementById("code").src="http://tdcaptcha.com/Models/ValidatorCode.php?t="+Math.random();"">换一组</a></span>';   
+    }else{
+        die("To use an right API key.");
+    }
+?>
