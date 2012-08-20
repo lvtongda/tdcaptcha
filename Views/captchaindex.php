166ffa61ca2d1e8646cad1da88051c5917be2f71
lvxiao <xiao.lv@upai.com>
2012-08-20 17:49:44 +0800

    update captchaindex.php

--- Views/captchaindex.php
+++ Views/captchaindex.php
@@ -1,5 +1,5 @@
 <?php
-if($pubkey = 'b7c06906733d7a4912d6d0d2faef5e50' || $pubkey == 'd800ae51a8af9790a5e9da430f@2b89'){
+if($pubkey = 'b7c06906733d7a4912d6d0d2faef5e50' || $pubkey = 'd800ae51a8af9790a5e9da430f@2b89' || $pubkey = 'ff3e266ba02dac2d2595cb3e0227516d'){
         echo '<meta http-equiv="Content-Type" content="text/html;charset=utf-8" />';
         echo '<img src="http://tdcaptcha.com/Models/ValidatorCode.php" id="code" /><br />';
         echo '<input type="text" value="" name="scode"><br />';
