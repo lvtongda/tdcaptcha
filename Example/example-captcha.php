f2b91de0ecd76fa227bcfb5a6f9097fe347823ed
lvxiao <xiao.lv@upai.com>
2012-08-21 10:28:50 +0800

    add "ypyun" support

--- Example/example-captcha.php
+++ Example/example-captcha.php
@@ -9,7 +9,10 @@ require_once('tdcaptchalib.php');
 $publickey = 'b7c06906733d7a4912d6d0d2faef5e50';
 
 //花瓣网（huaban.com）
-$publickey = 'd800ae51a8af9790a5e9da430f@2b89';
+//$publickey = 'd800ae51a8af9790a5e9da430f@2b89';
+
+//又拍云（www.upyun.com）
+//$publickey = 'ff3e266ba02dac2d2595cb3e0227516d';
 
 tdcaptcha_get_html($publickey);
 ?>
