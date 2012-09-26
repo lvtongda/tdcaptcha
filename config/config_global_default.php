a66ebfb9b9b3f784fe6f7f49d073fd966fb0f4ae
Xiao Lv <lvtongda@gmail.com>
2012-09-26 09:49:47 +0800

    the default config

---
+++ config/config_global_default.php
@@ -0,0 +1,8 @@
+<?php
+$con = mysql_connect("localhost", "username", "pwd");
+if(!$con) {
+    die('Could not connect:'.mysql_error());
+}
+
+mysql_select_db("dbname", $con);
+mysql_query("set names utf8");
