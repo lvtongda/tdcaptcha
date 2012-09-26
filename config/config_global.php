b39e6657b9698134df66758dde538efee20c2e02
Xiao Lv <lvtongda@gmail.com>
2012-09-26 09:52:49 +0800

    rm config_global.php

--- config/config_global.php
+++
@@ -1,8 +0,0 @@
-<?php
-$con = mysql_connect("localhost", "root", "zxfltt");
-if(!$con) {
-    die('Could not connect:'.mysql_error());
-}
-
-mysql_select_db("db_tdcaptcha", $con);
-mysql_query("set names utf8");
