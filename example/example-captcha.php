9dcbf9826273db36e39766d0ca31c0afabcc486c
Xiao Lv <lvtongda@gmail.com>
2012-10-09 17:47:34 +0800

    update example

--- example/example-captcha.php
+++ example/example-captcha.php
@@ -9,6 +9,23 @@ require_once('tdcaptchalib.php');
 $publickey = "026025a0bf4f95530552c6233d98c7cf";
 $privatekey = "702104ada24631a3eb913197a527c760";
 
+# the response from tdCAPTCHA
+$resp = null;
+$error = null;
+
+# was there a tdCAPTCHA response?
+if($_POST['tdcaptcha_response_field']) {
+    $resp = tdcaptcha_check_answer($privatekey,
+        $_POST['tdcaptcha_challenge_field'],
+        $_POST['tdcaptcha_response_field']);
+    
+    if($resp->is_valid) {
+        echo "You got it!";
+    }else {
+        # set the error code so that we can display it 
+        $error = $resp->error;
+    }
+}
 # Get a tdCAPTCHA response
 echo tdcaptcha_get_html($publickey);
 
--- example/tdcaptchalib.php
+++ example/tdcaptchalib.php
@@ -1,20 +1,57 @@
 <?php
 // This is a PHP library that handles calling tdCAPTCHA.
 
+# The tdCAPTCHA server URL's
+define("TDCAPTCHA_API_SERVER", "http://192.168.0.207/tdcaptcha");
+define("TDCAPTCHA_VERIFY_SERVER", "http://192.168.0.207/tdcaptcha");
+
+# Submits an HTTP POST to a tdCAPTCHA server
+function _tdcaptcha_http_post($url, $curlPost) {
+    $ch = curl_init();
+
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_HEADER, 1);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $curlPost);
+
+    $response = curl_exec($ch);
+    return $response;
+    curl_close($ch);
+}
+
 # The HTML to be embedded in the user's form.
 function tdcaptcha_get_html($pubkey) {
     if($pubkey == null || $pubkey == '') {
         die ("To use tdCAPTCHA you must get an API key.");    
     }
 
-    return '<img id="tdcaptcha_challenge_field" src="http://192.168.0.207/tdcaptcha/models/ValidatorCode.php" height="30" width="160" style="cursor:pointer" onclick="reloadcode()"><br />
+    $server = TDCAPTCHA_VERIFY_SERVER;
+
+    return '<img id="tdcaptcha_challenge_field" src="'.$server.'/models/ValidatorCode.php" height="30" width="160" style="cursor:pointer" onclick="reloadcode()"><br />
         <input type="hidden" value="" name="tdcaptcha_challenge_field">
         <input type="text" value="" name="tdcaptcha_response_field">
 
         <script type="text/javascript">
         function reloadcode() {
-            document.getElementById("tdcaptcha_challenge_field").src="http://192.168.0.207/tdcaptcha/models/ValidatorCode.php?"+Math.random();
+            document.getElementById("tdcaptcha_challenge_field").src="'.$server.'/models/ValidatorCode.php?"+Math.random();
         }
         </script>
         ';    
 }
+
+# A TdCaptchaResponse is returned from tdcaptcha_check_answer()
+class TdCaptchaResponse {
+    var $is_valid;
+    var $error;
+}
+
+# Calls an HTTP POST function to verify if the user's guess was correct
+function tdcaptcha_check_answer() {
+    if($privkey == null || $privkey == '') {
+        die("To use tdCAPTCHA you must get an API key.");
+    }
+
+    $response = _tdcaptcha_http_post()
+}
+
