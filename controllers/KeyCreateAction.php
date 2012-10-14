f2fe8ba8ff1039007c83182439ff99ee2566ad81
lvtongda <lvtongda@gmail.com>
2012-10-14 23:36:37 +0800

    update

--- controllers/KeyCreateAction.php
+++ controllers/KeyCreateAction.php
@@ -3,7 +3,6 @@ header("Content-Type:text/html; charset=utf-8");
 require_once('../config/config_global.php');
 
 $weburl = $_POST['weburl'];
-$ip = $_SERVER['REMOTE_ADDR'];
 $publickey = md5($_POST['weburl'].'publictdcaptcha');
 $privatekey = md5($_POST['weburl'].'privatetdcaptcha');
 
@@ -13,7 +12,7 @@ $result = mysql_query($sql_search);
 if(mysql_num_rows($result)) {
     exit('Domain name already exists!');
 }else {
-    $sql = "INSERT INTO db_tdcaptcha(weburl, ipaddress, publickey, privatekey) VALUES('$weburl', '$ip', '$publickey', '$privatekey')";
+    $sql = "INSERT INTO db_tdcaptcha(weburl, publickey, privatekey) VALUES('$weburl', '$publickey', '$privatekey')";
     if(mysql_query($sql)) {
         echo 'Domain Name: '.$weburl.'<br />';
         echo 'Public Key: '.$publickey.'<br />';
