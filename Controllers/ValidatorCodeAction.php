0c49a4d6091686c707fa31a7f37992189d97fe44
Xiao Lv <lvtongda@gmail.com>
2012-10-19 10:42:09 +0800

    update

--- Controllers/ValidatorCodeAction.php
+++
@@ -1,9 +0,0 @@
-<?php
-include("../Models/session_mysql.php");
-$code = $_SESSION['code'];
-$scode = $_POST['scode'];
-if($code == $scode){
-    echo "The captcha is right.";
-}else{
-    echo "The captcha is false.";
-}
